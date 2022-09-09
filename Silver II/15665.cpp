6c05af4070d9a5cab7de33f4d11740a1872bc4e2
Seungha Son <linuxias@gmail.com>
2022-09-09 17:52:14 +0900

    Create 15665.cpp

---
+++ Silver II/15665.cpp
@@ -0,0 +1,46 @@
+/*
+* https://www.acmicpc.net/problem/15665
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int res[9] = { 0, };
+int num[9];
+
+void find(int idx) {
+	if (idx == M) {
+		for (int i = 0; i < M; i++)
+			printf("%d ", res[i]);
+		printf("\n");
+		return;
+	}
+
+	int last = 0;
+
+	for (int i = 0; i < N; i++) {
+		if (last == num[i])
+			continue;
+
+		res[idx] = last = num[i];
+		find(idx + 1);
+	}
+}
+
+int main()
+{
+	scanf("%d %d", &N, &M);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &num[i]);
+
+	sort(num, num + N);
+
+	find(0);
+}
